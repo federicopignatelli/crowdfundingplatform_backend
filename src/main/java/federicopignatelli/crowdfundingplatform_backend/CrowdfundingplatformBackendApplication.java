025fc38951c4924b3641f140e5184721fd965f42
federicopignatelli <federicopignatelli@icloud.com>
2024-02-06 12:37:37 +0100

    Initial commit

---
+++ src/main/java/federicopignatelli/crowdfundingplatform_backend/CrowdfundingplatformBackendApplication.java
@@ -0,0 +1,13 @@
+package federicopignatelli.crowdfundingplatform_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdfundingplatformBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrowdfundingplatformBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/federicopignatelli/crowdfundingplatform_backend/CrowdfundingplatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package federicopignatelli.crowdfundingplatform_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrowdfundingplatformBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
