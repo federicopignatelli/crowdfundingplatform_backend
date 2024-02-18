a25a7f4118370352dd17493bc1a712dde1aeee5a
federicopignatelli <federicopignatelli@icloud.com>
2024-02-18 20:56:08 +0100

    fix

--- src/main/java/federicopignatelli/crowdfundingplatform_backend/security/SecurityConfig.java
+++ src/main/java/federicopignatelli/crowdfundingplatform_backend/security/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000/"));
 		configuration.setAllowedMethods(Arrays.asList("*"));
 		configuration.setAllowedHeaders(Arrays.asList("*"));
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
